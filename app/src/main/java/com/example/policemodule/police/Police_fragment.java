cfe34db67a1c762db68e1ece7a0af5c673667208
Nanduag0@ <nandini200400@gmail.com>
2020-01-25 01:48:33 +0530

    third commmit navigation drawer+changes

--- app/src/main/java/com/example/policemodule/police/Police_fragment.java
+++ app/src/main/java/com/example/policemodule/police/Police_fragment.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.policemodule.PoliceAdapter;
 import com.example.policemodule.R;
 
 import java.util.ArrayList;
@@ -35,7 +36,7 @@ public class Police_fragment extends Fragment
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View parent=inflater.inflate(R.layout.police_fragment,container,false);
         ButterKnife.bind(this,parent);
-      /*  infos=new ArrayList<>();
+        infos=new ArrayList<>();
         infos.add(new Police_Info(R.drawable.ic_launcher_background,"nrgjnf","jgnjrn","njgnjrg","jgrjgnfh0"));
         infos.add(new Police_Info(R.drawable.ic_launcher_background,"gjntth","jgnjnh","gkrnjrn","ngjnj"));
         infos.add(new Police_Info(R.drawable.ic_launcher_background,"gjntth","jgnjnh","gkrnjrn","ngjnj"));
@@ -44,7 +45,7 @@ public class Police_fragment extends Fragment
         infos.add(new Police_Info(R.drawable.ic_launcher_background,"gjntth","jgnjnh","gkrnjrn","ngjnj"));
         infos.add(new Police_Info(R.drawable.ic_launcher_background,"gjntth","jgnjnh","gkrnjrn","ngjnj"));
         infos.add(new Police_Info(R.drawable.ic_launcher_background,"gjntth","jgnjnh","gkrnjrn","ngjnj"));
-      */
+
 
         initrecycleadapter(getContext());
         return  parent;
