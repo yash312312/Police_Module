c2867210ed6fb8e90cbb8491dca698cad48b0efb
Nanduag0@ <nandini200400@gmail.com>
2020-01-31 13:35:13 +0530

    fresh commit

--- app/src/main/java/com/example/policemodule/login/LoginActivity.java
+++ app/src/main/java/com/example/policemodule/login/LoginActivity.java
@@ -6,8 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,18 +17,23 @@ import com.example.policemodule.MainActivity;
 import com.example.policemodule.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity
 {
+    @BindView(R.id.relative_layout)
+    RelativeLayout relativeLayout;
+
     @BindView(R.id.activity_login_tv_email)
     TextView email;
     @BindView(R.id.activity_login_tv_password)
-    TextView password;
+    TextView passwords;
     @BindView(R.id.activity_login_btn_login)
     Button login;
     @BindView(R.id.activity_login_tv_join)
@@ -55,11 +62,60 @@ public class LoginActivity extends AppCompatActivity
             }
         };
 
-        login.setOnClickListener(new View.OnClickListener() {
+
+      /*  login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                String msg;
+                String emailId=email.getText().toString();
+                String password=passwords.getText().toString();
+                if(emailId.isEmpty() || password.isEmpty())
+                {
+                    msg="Fields are empty";
+                    showSnackbar(msg);
+                    return;
+                }
+                if((!Patterns.EMAIL_ADDRESS.matcher(emailId).matches()))
+                {
+                    msg="Email is Invalid";
+                    showSnackbar(msg);
+                    return;
+                }
+                if(password.length()<=6)
+                {
+                    msg="Password should be longer than 6 character";
+                    showSnackbar(msg);
+                    return;
+                }
+                Intent i = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+            //TODO extract the string from the edit texts and validate the strings . Check whether the entered email is a valid email or not
+            // check whether the password is longer than 6 characters . Also if the password is not that long prepare a SnackBar to dislay
+            // the error.Once everything is correct jump to the next activity.
+        }
+        public void showSnackbar(String msg)
+        {
+            Snackbar snackbar= Snackbar.make(relativeLayout,msg,Snackbar.LENGTH_INDEFINITE)
+                    .setAction("UNDO", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Snackbar snackbar1=Snackbar.make(relativeLayout,"Undo Successful",Snackbar.LENGTH_SHORT);
+                            snackbar1.show();
+                        }
+                    });
+            snackbar.show();
+        }*/
+
+         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String emailId = email.getText().toString();
-                String pwd = password.getText().toString();
+                String pwd = passwords.getText().toString();
                 if (TextUtils.isEmpty(emailId)) {
                     Toast.makeText(LoginActivity.this, "Plese enter valid email", Toast.LENGTH_SHORT).show();
                 }
--- app/src/main/java/com/example/policemodule/map/Map_Fragment.java
+++ app/src/main/java/com/example/policemodule/map/Map_Fragment.java
@@ -1,18 +1,52 @@
 package com.example.policemodule.map;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.policemodule.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Map_Fragment extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+
+    public Map_Fragment() {
+        // Required empty public constructor
     }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.map_fragment, container, false);
+        ButterKnife.bind(this,view);
+
+        return view;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+
+
+        super.onAttach(context);
+    }
+
+
+
 }
